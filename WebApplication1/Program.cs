b64a0c2be7a7368472cbc75fa47166f71b06fe90
calebxzhou <me@calebxzhou.cn>
2023-12-19 11:38:48 +0800

    q

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,58 @@
 using System.Text.Json;
+using SharpDocx;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add CORS services.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+builder.Services.AddEndpointsApiExplorer(); 
+var app = builder.Build();
+
+var jsonOptions = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true,
+};
+
+// Configure the HTTP request pipeline.
+ 
+app.UseHttpsRedirection();
+ 
+app.UseCors(b => b
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+app.MapPost("/pjtest_do", async (HttpRequest req) => 
+{
+    // Read the request body as a string
+    var bodyString = await new StreamReader(req.Body).ReadToEndAsync();
+    Console.WriteLine(bodyString);
+    // Deserialize the string into a PjTest object
+    var modules = JsonSerializer.Deserialize<List<ModuleTest>>(bodyString,jsonOptions)
+                  ?? new List<ModuleTest>();
+    var pjTest = new PjTest(modules);
+    var document = DocumentFactory.Create("pjtest.docx",modules);
+    document.Generate("gen.docx");
+    for (var i = 0; i < modules.Count; i++)
+    {
+        
+    }
+    // Now you can use the deserialized 'pjTest' object
+    // ...
+
+    return Results.Ok();
+});
+
+app.Run();
+
 [Serializable]
 public record TestCase(string Name, string Operation, string Result);
 // 测试项
@@ -18,25 +72,5 @@ public record PjTest(List<ModuleTest> Modules)
     // 全部模块测试
     
 }
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer(); 
-var app = builder.Build();
-var jsonOptions = new JsonSerializerOptions
-{
-    PropertyNameCaseInsensitive = true,
-};
-// Configure the HTTP request pipeline.
- 
-app.UseHttpsRedirection();
- 
-app.MapGet("/pjtest_do", async context =>
-    {
-        await context.Response.WriteAsync("Hello World");
-    })
-    .WithName("index")
-    .WithOpenApi();
 
-app.Run();
